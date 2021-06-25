1064ed2bae4d4425100cf50458c9d169c162c3c6
Serhii Tomashkevich <19tomat@gmail.com>
2021-06-26 00:20:28 +0300

    +add:component Profile

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Profile from './Profile/Profile';
+import user from './user/user'
+
+const App = () => {
+  return <Profile
+    name={user.name}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />;
+};
+
+export default App;
